129af7b042432e3f540b69cb88fa2d30c51f283d
taemin <tamin0112@gmail.com>
2023-02-16 17:59:02 +0900

    23-02-16 commit

--- src/main/java/com/simple/basic/command/SimpleVO2.java
+++ src/main/java/com/simple/basic/command/SimpleVO2.java
@@ -1,19 +1,16 @@
 package com.simple.basic.command;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class SimpleVO {
+@AllArgsConstructor
+public class SimpleVO2 {
 	
-	private int userNum;
-	private String userName;
+	private String Num;
+	private String Name;
 	private String id;
-	
 
 }
